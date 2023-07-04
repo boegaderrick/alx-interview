201ce62fae5e786f5c90732e479a7122653a4489
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-07-04 17:12:17 +0300

    check if lockboxes can be unlocked

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This module contains a function that solves the lockbox challenge"""
+
+
+def canUnlockAll(boxes):
+    """
+        This function checks to see if all the lockboxes in the passed
+        parameter can be opened using keys contained in the lockboxes
+    """
+    keys = [False if i > 0 else True for i in range(len(boxes))]
+    remaining = []
+
+    for i in range(len(boxes)):
+        if keys[i] is False:
+            remaining.append(i)
+            continue
+        for key in boxes[i]:
+            keys[key] = True
+
+    for i in remaining:
+        if keys[i] is False:
+            return False
+        for key in boxes[i]:
+            keys[key] = True
+
+    return True
