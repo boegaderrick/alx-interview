199c92887ec1edd9c1c03d0acff8415b8d7cbf68
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-07-14 00:50:31 +0300

    solve minimum operations challenge

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,18 +2,18 @@
 """This module contains solutions to the minimum operations challenge"""
 
 
-def lFactor(n: int) -> int:
+def lFactor(n):
     """This function finds the largest factor of a number"""
-    i: int = 2
+    i = 2
     while True:
         if n % i == 0:
             return int(n / i)
         i += 1
 
 
-def minOperations(n: int) -> int:
+def minOperations(n):
     """This function contains the logic to solve the challenge"""
     if n < 2:
         return 0
-    f: int = lFactor(n)
+    f = lFactor(n)
     return int((n / f) + minOperations(f))
