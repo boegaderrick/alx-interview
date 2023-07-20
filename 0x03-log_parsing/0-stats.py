98a8653d1ad11b6ad4e7f7cbad509c1b1b79663e
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-07-20 19:09:32 +0300

    log parsing - alx interview

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,36 +1,37 @@
 #!/usr/bin/python3
-"""This module parses logs and outputs metrics"""
+"""This script parses logs and outputs metrics"""
 import sys
 
-fileSize = 0
-codeCount = {}
-iteration = 0
+if __name__ == '__main__':
+    fileSize = 0
+    codeCount = {}
+    iteration = 0
 
 
-def printCodes():
-    """This function handles the output"""
-    print('File size: {}'.format(fileSize))
-    for code, count in sorted(codeCount.items()):
-        print('{}: {}'.format(code, count))
+    def printCodes():
+        """This function handles the output"""
+        print('File size: {}'.format(fileSize))
+        for code, count in sorted(codeCount.items()):
+            print('{}: {}'.format(code, count))
 
 
-while True:
-    try:
-        line = sys.stdin.readline()
-        splitLineList = line.split(' ')
-        if len(splitLineList) < 9:
-            continue
+    while True:
+        try:
+            line = sys.stdin.readline()
+            splitLineList = line.split(' ')
+            if len(splitLineList) < 9:
+                continue
 
-        fileSize, code = fileSize + int(splitLineList[-1]), splitLineList[-2]
-        if not code.isdecimal():
-            continue
+            fileSize, code = fileSize + int(splitLineList[-1]), splitLineList[-2]
+            if not code.isdecimal():
+                continue
 
-        codeCount[code] = codeCount[code] + 1 if codeCount.get(code) else 1
-        iteration += 1
-        if iteration == 10:
+            codeCount[code] = codeCount[code] + 1 if codeCount.get(code) else 1
+            iteration += 1
+            if iteration == 10:
+                printCodes()
+                iteration = 0
+
+        except KeyboardInterrupt as error:
             printCodes()
-            iteration = 0
-
-    except KeyboardInterrupt as error:
-        printCodes()
-        exit(1)
+            exit(1)
