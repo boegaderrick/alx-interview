bd2fdb0a7761370657cc779d365ca6973872da89
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-07-20 22:46:20 +0300

    log parsing - alx interview

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -9,8 +9,6 @@ if __name__ == '__main__':
 
     def printCodes():
         """This function handles the output"""
-        if len(codeCount) < 1:
-            return
         print('File size: {}'.format(fileSize))
         for code, count in sorted(codeCount.items()):
             print('{}: {}'.format(code, count))
@@ -19,12 +17,13 @@ if __name__ == '__main__':
         try:
             line = sys.stdin.readline()
             if not line:
-                if iteration > 0:
+                if iteration > 0 or (iteration == 0 and len(codeCount) == 0):
                     printCodes()
                 exit(0)
             splitLine = line.split(' ')
             if len(splitLine) < 9:
-                continue
+                if not all(i in splitLine for i in ['"GET', 'HTTP/1.1"']):
+                    continue
 
             fileSize, code = fileSize + int(splitLine[-1]), splitLine[-2]
             if not code.isdecimal():
