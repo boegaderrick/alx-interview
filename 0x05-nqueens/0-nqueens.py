d39c1f281c1efa658a865cf2464e210c7d2dfe1f
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-08-04 04:38:46 +0300

    nqueens challenge

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,80 +1,45 @@
 #!/usr/bin/python3
-"""This module contains functions that solve the nqueens challenge"""
+"""This module contains a function that solves the nqueens challenge"""
 import sys
 
 
-def nqueens(n):
-    """Main function"""
-    positions = [[]]
-    idx, row, pos = 0, 0, 0
-    while row < n:
-        while pos < n:
-            if posIsValid(positions[idx], row, pos, n):
-                positions[idx].append([row, pos])
-                break
-            pos += 1
-        if len(positions[idx]) == n and positions[idx][0][1] < n - 1:
-            row = positions[idx][0][0]
-            pos = positions[idx][0][1] + 1
-            positions.append([])
-            idx += 1
+def nqueens(x):
+    """
+        Finds all possible safe queen positioning on an NxN
+        chessboard using backtracking
+    """
+    if x == n:
+        print(indices)
+        return
+    for i in range(n):
+        if i in columns or x + i in diag1 or x - i in diag2:
             continue
+        columns.add(i)
+        diag1.add(x + i)
+        diag2.add(x - i)
+        indices.append([x, i])
+        nqueens(x + 1)
 
-        try:
-            lastInsert = positions[idx][len(positions[idx]) - 1]
-            if lastInsert[0] != row:
-                positions[idx].pop()
-                row = lastInsert[0]
-                pos = lastInsert[1] + 1
-                continue
-        except Exception:
-            pass
-        row += 1
-        pos = 0
-    printPositions(positions, n)
-
-
-def posIsValid(positions, row, pos, n):
-    """Checks if a position is safe"""
-    tRow, tPos = row - 1, pos - 1
-    while tRow >= 0 and tPos >= 0:
-        if [tRow, tPos] in positions:
-            return False
-        tRow -= 1
-        tPos -= 1
-
-    tRow, tPos = row - 1, pos
-    while tRow >= 0:
-        if [tRow, tPos] in positions:
-            return False
-        tRow -= 1
-
-    tRow, tPos = row - 1, pos + 1
-    while tRow >= 0 and tPos < n:
-        if [tRow, tPos] in positions:
-            return False
-        tRow -= 1
-        tPos += 1
-
-    return True
-
-
-def printPositions(positions, n):
-    """Prints the list of possible arrangements"""
-    for pos in positions:
-        if len(pos) == n:
-            print(pos)
+        columns.remove(i)
+        diag1.remove(x + i)
+        diag2.remove(x - i)
+        indices.remove([x, i])
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('Usage: nqueens N')
         exit(1)
-    n = sys.argv[1]
-    if not n.isdecimal():
+    if not sys.argv[1].isdecimal():
         print('N must be a number')
         exit(1)
-    if int(n) < 4:
+    if int(sys.argv[1]) < 4:
         print('N must be at least 4')
         exit(1)
-    nqueens(int(n))
+    n = int(sys.argv[1])
+    columns = set()
+    diag1 = set()
+    diag2 = set()
+    indices = []
+
+    nqueens(0)
