625135c7b4973199fe6b4c86458dde3169571ec6
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-08-23 19:26:01 +0300

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,12 +4,14 @@
 
 def makeChange(coins, total):
     """Solution logic"""
+    if total <= 0:
+        return 0
     coins.sort(reverse=True)
     numCoins = 0
     for coin in coins:
-        if total <= 0:
-            break
         numCoins += total // coin
         total %= coin
+        if total == 0:
+            break
 
     return numCoins if numCoins and not total else -1
